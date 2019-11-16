eb75e479fdd65c7b03b0433b546ed42cd221a8fd
jt851113 <eddielaio@DESKTOP-S5SPJOO.localdomain>
2019-11-16 15:28:08 +0800

    update content

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ file_folder = '/mnt/c/Users/eddielaio/Project/IR_project1/dataset/*'
 idx = 1
 files_with_index = {}
 for file in glob.glob(file_folder):
-    #print(file)
+    # print(file)
     fname = file
     file = open(file, "r")
     text = file.read()
@@ -71,17 +71,17 @@ unique_words_all = set(dict_global.keys())
 linked_list_data = {}
 for word in unique_words_all:
     linked_list_data[word] = SlinkedList()
-    linked_list_data[word].head = Node(1,Node)
+    linked_list_data[word].head = Node(1, Node)
 word_freq_in_doc = {}
 idx = 1
 for file in glob.glob(file_folder):
     file = open(file, "r")
     text = file.read()
     text = remove_special_characters(text)
-    text = re.sub(re.compile('\d'),'',text)
+    text = re.sub(re.compile('\d'), '', text)
     sentences = sent_tokenize(text)
     words = word_tokenize(text)
-    words = [word for word in words if len(words)>1]
+    words = [word for word in words if len(words) > 1]
     words = [word.lower() for word in words]
     words = [word for word in words if word not in Stopwords]
     word_freq_in_doc = finding_all_unique_words_and_freq(words)
@@ -89,65 +89,71 @@ for file in glob.glob(file_folder):
         linked_list = linked_list_data[word].head
         while linked_list.nextval is not None:
             linked_list = linked_list.nextval
-        linked_list.nextval = Node(idx ,word_freq_in_doc[word])
+        linked_list.nextval = Node(idx, word_freq_in_doc[word])
     idx = idx + 1
 
+# TODO:change input method
 
-query = input('Enter your query:')
-query = word_tokenize(query)
-connecting_words = []
-cnt = 1
-different_words = []
-for word in query:
-    if word.lower() != "and" and word.lower() != "or" and word.lower() != "not":
-        different_words.append(word.lower())
-    else:
-        connecting_words.append(word.lower())
-print(connecting_words)
-total_files = len(files_with_index)
-zeroes_and_ones = []
-zeroes_and_ones_of_all_words = []
-for word in (different_words):
-    if word.lower() in unique_words_all:
-        zeroes_and_ones = [0] * total_files
-        linkedlist = linked_list_data[word].head
-        print(word)
-        while linkedlist.nextval is not None:
-            zeroes_and_ones[linkedlist.nextval.doc - 1] = 1
-            linkedlist = linkedlist.nextval
-        zeroes_and_ones_of_all_words.append(zeroes_and_ones)
-    else:
-        print(word," not found")
-        sys.exit()
-print(zeroes_and_ones_of_all_words)
-for word in connecting_words:
-    word_list1 = zeroes_and_ones_of_all_words[0]
-    word_list2 = zeroes_and_ones_of_all_words[1]
-    if word == "and":
-        bitwise_op = [w1 & w2 for (w1,w2) in zip(word_list1,word_list2)]
-        zeroes_and_ones_of_all_words.remove(word_list1)
-        zeroes_and_ones_of_all_words.remove(word_list2)
-        zeroes_and_ones_of_all_words.insert(0, bitwise_op);
-    elif word == "or":
-        bitwise_op = [w1 | w2 for (w1,w2) in zip(word_list1,word_list2)]
-        zeroes_and_ones_of_all_words.remove(word_list1)
-        zeroes_and_ones_of_all_words.remove(word_list2)
-        zeroes_and_ones_of_all_words.insert(0, bitwise_op);
-    elif word == "not":
-        bitwise_op = [not w1 for w1 in word_list2]
-        bitwise_op = [int(b == True) for b in bitwise_op]
-        zeroes_and_ones_of_all_words.remove(word_list2)
-        zeroes_and_ones_of_all_words.remove(word_list1)
-        bitwise_op = [w1 & w2 for (w1,w2) in zip(word_list1,bitwise_op)]
-zeroes_and_ones_of_all_words.insert(0, bitwise_op);
-        
-files = []    
-print(zeroes_and_ones_of_all_words)
-lis = zeroes_and_ones_of_all_words[0]
-cnt = 1
-for index in lis:
-    if index == 1:
-        files.append(files_with_index[cnt])
-    cnt = cnt+1
-    
-print(files)
+try:
+    while True:
+        query = input('Enter your query:')
+        query = word_tokenize(query)
+        connecting_words = []
+        cnt = 1
+        different_words = []
+        for word in query:
+            if word.lower() != "and" and word.lower() != "or" and word.lower() != "not":
+                different_words.append(word.lower())
+            else:
+                connecting_words.append(word.lower())
+        #print(connecting_words)
+        total_files = len(files_with_index)
+        zeroes_and_ones = []
+        zeroes_and_ones_of_all_words = []
+        for word in (different_words):
+            if word.lower() in unique_words_all:
+                zeroes_and_ones = [0] * total_files
+                linkedlist = linked_list_data[word].head
+                print(word)
+                while linkedlist.nextval is not None:
+                    zeroes_and_ones[linkedlist.nextval.doc - 1] = 1
+                    linkedlist = linkedlist.nextval
+                zeroes_and_ones_of_all_words.append(zeroes_and_ones)
+            else:
+                print(word, " not found")
+        #print(zeroes_and_ones_of_all_words)
+        for word in connecting_words:
+            word_list1 = zeroes_and_ones_of_all_words[0]
+            word_list2 = zeroes_and_ones_of_all_words[1]
+            if word == "and":
+                bitwise_op = [w1 & w2 for (w1, w2) in zip(
+                    word_list1, word_list2)]
+                zeroes_and_ones_of_all_words.remove(word_list1)
+                zeroes_and_ones_of_all_words.remove(word_list2)
+                zeroes_and_ones_of_all_words.insert(0, bitwise_op)
+            elif word == "or":
+                bitwise_op = [w1 | w2 for (w1, w2) in zip(
+                    word_list1, word_list2)]
+                zeroes_and_ones_of_all_words.remove(word_list1)
+                zeroes_and_ones_of_all_words.remove(word_list2)
+                zeroes_and_ones_of_all_words.insert(0, bitwise_op)
+            elif word == "not":
+                bitwise_op = [not w1 for w1 in word_list2]
+                bitwise_op = [int(b == True) for b in bitwise_op]
+                zeroes_and_ones_of_all_words.remove(word_list2)
+                zeroes_and_ones_of_all_words.remove(word_list1)
+                bitwise_op = [w1 & w2 for (w1, w2) in zip(
+                    word_list1, bitwise_op)]
+        zeroes_and_ones_of_all_words.insert(0, bitwise_op)
+
+        files = []
+        # print(zeroes_and_ones_of_all_words)
+        lis = zeroes_and_ones_of_all_words[0]
+        cnt = 1
+        for index in lis:
+            if index == 1:
+                files.append(files_with_index[cnt])
+            cnt = cnt+1
+        print(files)
+except EOFError:
+    pass
