0c3bd4475d01fa6533850f564c8379dca5729d9e
SupreSky <nanan2468@yahoo.com.tw>
2019-11-18 17:06:43 +0800

    first commit, with linked list bugs
    linked list bugs waiting for AD to fix

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 import glob
 import re
 import os
 import numpy as np
 import sys
+
 Stopwords = set(stopwords.words('english'))
+stemmer = SnowballStemmer("english")
 
-
-def finding_all_unique_words_and_freq(words):
+def finding_unique(words):
     words_unique = []
-    word_freq = {}
+    
     for word in words:
         if word not in words_unique:
             words_unique.append(word)
+    return words_unique
+
+def finding_freq(words_unique):
+    words_freq = {}
     for word in words_unique:
-        word_freq[word] = words.count(word)
-    return word_freq
+        words_freq[word] = words.count(word)
+    return words_freq
 
-
+"""
 def finding_freq_of_word_in_doc(word, words):
     freq = words.count(word)
-
+"""
 
 def remove_special_characters(text):
     regex = re.compile('[^a-zA-Z0-9\s]')
@@ -45,7 +50,7 @@ class SlinkedList:
 
 all_words = []
 dict_global = {}
-file_folder = '/mnt/c/Users/eddielaio/Project/IR_project1/dataset/*'
+file_folder = 'dataset/*'
 idx = 1
 files_with_index = {}
 for file in glob.glob(file_folder):
@@ -60,10 +65,15 @@ for file in glob.glob(file_folder):
     words = [word for word in words if len(words) > 1]
     words = [word.lower() for word in words]
     words = [word for word in words if word not in Stopwords]
-    dict_global.update(finding_all_unique_words_and_freq(words))
+    words_stemmed = [stemmer.stem(x) for x in words]
+    words_freq = finding_freq(finding_unique(words_stemmed))
+    dict_global.update(words_freq)
     files_with_index[idx] = os.path.basename(fname)
     idx = idx + 1
-
+"""
+for i, (key, value) in enumerate( sorted(dict_global.items(), key=lambda x: x[1]) ):
+    print("%d, %s: %s" % (i, key, value))
+"""
 unique_words_all = set(dict_global.keys())
 
 # index construct
@@ -84,8 +94,10 @@ for file in glob.glob(file_folder):
     words = [word for word in words if len(words) > 1]
     words = [word.lower() for word in words]
     words = [word for word in words if word not in Stopwords]
-    word_freq_in_doc = finding_all_unique_words_and_freq(words)
+    words_stemmed = [stemmer.stem(x) for x in words]
+    words_freq = finding_freq(finding_unique(words_stemmed))
     for word in word_freq_in_doc.keys():
+        print(w)
         linked_list = linked_list_data[word].head
         while linked_list.nextval is not None:
             linked_list = linked_list.nextval
@@ -93,6 +105,8 @@ for file in glob.glob(file_folder):
     idx = idx + 1
 
 # TODO:change input method
+
+print(linked_list_data)
 
 try:
     while True:
